a86c2f1fe7d9214ff4356d96b76e92d003c7a1c6
lai3392 <141746079+lai3392@users.noreply.github.com>
2023-12-03 15:39:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "本当に？！ありがとう！";
   gif.src =
     "https://media.giphy.com/media/YIYhgGXqqyTRJog7AM/giphy.gif";
 });
